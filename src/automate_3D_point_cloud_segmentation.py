c08a8d98c33fd158bafa4cd8dc76159739d5c7f2
Denis Potapov <potapovdenisdmit@gmail.com>
2022-05-03 23:15:45 +0500

    cloud segmentation

--- src/automate_3D_point_cloud_segmentation.py
+++ src/automate_3D_point_cloud_segmentation.py
@@ -50,40 +50,86 @@ def one_function_clustering():
 
     segment_models = {}  # сохраняем сюда параметры a, b, c найденных плоскостей
     segments = {}
-    max_plane_idx = 20  # количество плоскостей, которые будем искать
-    d_threshold = 0.01
+    centroids = {}
+    max_plane_idx = [20]  # количество плоскостей, которые будем искать
+    plane_distance_threshold = [0.01, 0.02]
+    dbscan_threshold = [0.01, 0.02]
 
-    rest = pcd
-    for i in tqdm(range(max_plane_idx)):
-        colors = plt.get_cmap("tab20")(i)
-        segment_models[i], inliers = rest.segment_plane(
-            distance_threshold=0.01, ransac_n=3, num_iterations=1000)
-        segments[i] = rest.select_by_index(inliers)
-        labels = np.array(segments[i].cluster_dbscan(eps=d_threshold * 10, min_points=10))
-        candidates = [len(np.where(labels == j)[0]) for j in np.unique(labels)]
-        best_candidate = int(np.unique(labels)[np.where(candidates == np.max(candidates))[0]])
+    wcss_results = {}
 
-        rest = rest.select_by_index(inliers, invert=True) + segments[i].select_by_index(
-            list(np.where(labels != best_candidate)[0]))
-        segments[i] = segments[i].select_by_index(list(np.where(labels == best_candidate)[0]))
+    count_of_experimrnts = len(max_plane_idx) * len(plane_distance_threshold) * len(dbscan_threshold)
+    current_experiment = 0
 
-        segments[i].paint_uniform_color(list(colors[:3]))
-        # rest = rest.select_by_index(inliers, invert=True)
+    # будем искать лучшие параметры для кластеризации
+    for plane_idx in max_plane_idx:
+        for dbscan_th in dbscan_threshold:
+            for plane_distance_th in plane_distance_threshold:
 
-    labels = np.array(rest.cluster_dbscan(eps=0.05, min_points=5))
-    max_label = labels.max()
-    print(f"point cloud has {max_label + 1} clusters")
-    colors = plt.get_cmap("tab10")(labels / (max_label if max_label > 0 else 1))
-    colors[labels < 0] = 0
-    rest.colors = o3d.utility.Vector3dVector(colors[:, :3])
+                rest = pcd
+                for i in tqdm(range(plane_idx)):
+                    colors = plt.get_cmap("tab20")(i)
+                    # с помощью RANSAC находим поддерживающие плоскости
+                    segment_models[i], inliers = rest.segment_plane(
+                        distance_threshold=plane_distance_th, ransac_n=3, num_iterations=1000)
+                    # назначаем найденным сегментом все невыбросы, а найденную плоскость
+                    segments[i] = rest.select_by_index(inliers)
 
-    o3d.visualization.draw_geometries([segments[i] for i in range(max_plane_idx)] + [rest],
-                                      width=1080,
-                                      height=720,
-                                      zoom=0.3199,
-                                      front=[0.30159062875123849, 0.94077325609922868, 0.15488309545553303],
-                                      lookat=[-3.9559999108314514, -0.055000066757202148, -0.27599999308586121],
-                                      up=[-0.044411423633999815, -0.138726419067636, 0.98753122516983349])
+                    # в найденной плоскости делаем кластеризацию т.к. эта плоскость может идти через всю картину
+                    # и в одной плоскости могут лежать точки из разных частей 3D сцены
+                    # используем алгоритм DBSCAN, которому передаем на каком расстоянии искать точки одного
+                    # кластера и сколько соседних точек должно быть.
+                    labels = np.array(segments[i].cluster_dbscan(eps=dbscan_th * 10, min_points=10))
+
+                    # находим количество точек в каждом кластере
+                    candidates = [len(np.where(labels == j)[0]) for j in np.unique(labels)]
+                    # находим метку кластера с наибольшим количеством точек
+                    best_candidate = int(np.unique(labels)[np.where(candidates == np.max(candidates))[0]])
+
+                    # в оставшиеся точки (которые не попали на плоскость) добавляем и точки нашей плоскости,
+                    # которые не входят в самый большой кластер
+                    rest = rest.select_by_index(inliers, invert=True) + segments[i].select_by_index(
+                        list(np.where(labels != best_candidate)[0]))
+
+                    # итоговый сегмент это все точки самого большого кластера
+                    segments[i] = segments[i].select_by_index(list(np.where(labels == best_candidate)[0]))
+                    # красим точки сегмента
+                    segments[i].paint_uniform_color(list(colors[:3]))
+
+                    # считаем центроид кластера, он нужен будет для суммы расчета квадратов расстояний
+                    # центроида и всех точек кластера
+                    centroids[i] = np.mean(np.asarray(segments[i].points), axis=0)
+
+                # рассчитываем сумму квадратов расстояний от центроида каждого кластера до его точек
+                wcss = 0
+                for segment, centroid in zip(segments.values(), centroids.values()):
+                    wcss += np.sum(np.linalg.norm(centroid - np.asarray(segment.points), axis=1))
+
+                # сохраняем для постороения графика
+                wcss_results[f"pc_{plane_idx}_dpth{plane_distance_th}_dbth_{dbscan_th}_"] = wcss
+
+                # оставшиеся точки после поиска всех плоскостей кластеризируем с помощью DBSCAN
+                labels = np.array(rest.cluster_dbscan(eps=0.05, min_points=5))
+                max_label = labels.max()
+                colors = plt.get_cmap("tab10")(labels / (max_label if max_label > 0 else 1))
+                colors[labels < 0] = 0
+                rest.colors = o3d.utility.Vector3dVector(colors[:, :3])
+
+                # o3d.visualization.draw_geometries([segments[i] for i in range(max_plane_idx)] + [rest],
+                #                                   width=1080,
+                #                                   height=720,
+                #                                   zoom=0.3199,
+                #                                   front=[0.30159062875123849, 0.94077325609922868, 0.15488309545553303],
+                #                                   lookat=[-3.9559999108314514, -0.055000066757202148, -0.27599999308586121],
+                #                                   up=[-0.044411423633999815, -0.138726419067636, 0.98753122516983349])
+                current_experiment += 1
+                print(f"Experiment {current_experiment}/{count_of_experimrnts}")
+
+    # строим график зависимости параметров от суммы квадратов расстояний кластеров
+    plt.plot(list(wcss_results.keys()), list(wcss_results.values()))
+    plt.xticks(rotation=90)
+    plt.xlabel("Number of clusters")
+    plt.ylabel("WCSS")
+    plt.show()
 
 
 if __name__ == "__main__":
